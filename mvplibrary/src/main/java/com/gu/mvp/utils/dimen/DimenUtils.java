09c0ecee19d957aeb39eb5d1c6d60be88f6f5884
gloriousgu <gloriousgu@126.com>
2018-05-18 18:14:51 +0800

    tmp

--- mvplibrary/src/main/java/com/gu/mvp/utils/dimen/DimenUtils.java
+++ mvplibrary/src/main/java/com/gu/mvp/utils/dimen/DimenUtils.java
@@ -1,7 +1,10 @@
 package com.gu.mvp.utils.dimen;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 
 public class DimenUtils {
@@ -38,5 +41,36 @@ public class DimenUtils {
       result = context.getResources().getDimensionPixelSize(resourceId);
     }
     return result;
+  }
+
+  /**
+   * 判断当前sdk版本是否大于指定版本
+   *
+   * @param version 指定版本
+   * @return 是否大于
+   */
+  public static boolean currentSdkUpVersion(final int version) {
+    return Build.VERSION.SDK_INT >= version;
+  }
+
+  /**
+   * 设置toolbar padding和height，设置该方法可去除android:fitsSystemWindows="true"
+   *
+   * @param context 上下文
+   * @param toolbar 当前toolbar
+   */
+  public static void setToolbarFitsSystemWindows(Context context, Toolbar toolbar) {
+    ViewGroup.LayoutParams params = toolbar.getLayoutParams();
+    int paddingTop = DimenUtils.getStatusBarHeight(context);
+    if (paddingTop == 0) {
+      try {
+        throw new Exception("can't get status bar height ! Error !");
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    params.height = paddingTop + toolbar.getHeight();
+    toolbar.setPadding(0, paddingTop, 0, 0);
+    toolbar.setLayoutParams(params);
   }
 }
